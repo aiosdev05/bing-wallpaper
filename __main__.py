0d3b25c3438aa037c91cf10c75c67c3f3c9d824c
Brian Wing <brian.wing@sisinc.org>
2018-09-04 10:44:11 -0400

    Found username dynamically and create backgrounds folder if missing

--- __main__.py
+++ __main__.py
@@ -1,18 +1,23 @@
 """
 Author: Brian Wing
-Version: 2018.6.11.1
+Version: 2018.9.4
 License: Apache
 """
 
 import urllib.request
 import json
 import ctypes
+from pathlib import Path
+import os
+import getpass
 
 base_url = "https://www.bing.com/"
 json_url = "HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"  # This is the address where Bing keeps the info for its images
 img_url = ""
 img_name = ""
-save_loc = "C:\\Users\\Brian\\Pictures\\Bing Backgrounds\\"
+save_loc = "C:\\Users\\" + getpass.getuser() + "\\Pictures\\Bing Backgrounds\\"
+
+print("Finding today's image...")
 
 # Get JSON info about what the path the image for today is
 with urllib.request.urlopen(base_url + json_url) as url:
@@ -20,6 +25,13 @@ with urllib.request.urlopen(base_url + json_url) as url:
     img_url = data["images"][0]["url"]  # Get today's image url postfix. This should be something like /az/hprichbg/rb/GBRBday_EN-US12873687095_1920x1080.jpg
     img_url_pieces = img_url.split("/")
     img_name = img_url_pieces[len(img_url_pieces) - 1]  # The name of the image is the last part of the img_url
+
+background_folder = Path(save_loc)
+if not background_folder.exists():
+    print("Creating backgrounds folder at \"" + background_folder + "\"...")
+    os.makedirs(background_folder)  # background_folder doesn't exists, create it
+
+print("Downloading image...")
 
 urllib.request.urlretrieve(base_url + img_url, save_loc + img_name)  # Download img from Bing and save in save location
 ctypes.windll.user32.SystemParametersInfoW(20, 0, save_loc + img_name, 0)  # Set the downloaded image as the wallpaper
